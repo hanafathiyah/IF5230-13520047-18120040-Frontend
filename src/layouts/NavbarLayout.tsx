11e2cccacb5f498a37a06fe5d61a2ea084857af1
Hana Fathiyah <13520047@std.stei.itb.ac.id>
2024-05-19 09:32:18 +0700

    update chatbot

--- src/layouts/NavbarLayout.tsx
+++ src/layouts/NavbarLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../component/Navbar/Navbar";
+import Navbar from "../component/ResponsiveAppBar/Navbar";
 
 function NavbarLayout() {
   return (
--- src/routes/Chatbot.tsx
+++ src/routes/Chatbot.tsx
@@ -3,9 +3,16 @@ import { useEffect, useState } from "react";
 import "axios";
 import axios from "axios";
 import { Box, Button, Grid, Stack, TextField, Typography } from "@mui/material";
+
+interface IQA {
+  question: string;
+  answer: string;
+}
 const Chatbot = () => {
   const [question, setQuestion] = useState<string>("");
-  const [answer, setAnswer] = useState<string>("");
+  // const [answer, setAnswer] = useState<string>("");
+  const [arrQA, setArrQA] = useState<IQA[]>([]);
+
   const handleClickRegex = () => {
     axios
       .post("http://localhost:5000/chatbot_regex", {
@@ -13,7 +20,8 @@ const Chatbot = () => {
       })
       .then((response) => {
         console.log(response);
-        setAnswer(response.data);
+        // setAnswer(response.data);
+        setArrQA([...arrQA, { question: question, answer: response.data }]);
       })
       .catch((error) => {
         console.error(error);
@@ -48,27 +56,175 @@ const Chatbot = () => {
         style={{ minHeight: "100vh" }}
         sx={{ backgroundColor: "#17161D" }}
       >
-        <Typography sx={{ marginBottom: 2 }}>Chatbot</Typography>
-        <Stack direction={"row"} spacing={2}>
-          <Box
-            sx={{
-              bgcolor: "white",
-              padding: 2,
-              borderRadius: "5%",
-              margin: 2,
-              border: 1,
-            }}
-          >
-            <Typography sx={{ color: "black" }}>Chat</Typography>
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              onChange={(e) => handleInputQuestion(e.target.value)}
-            />
-            <Button onClick={handleClickRegex}>sent</Button>
-            <Typography>{answer}</Typography>
-          </Box>
-        </Stack>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Grid item xs={3}>
+            <Box>
+              <Stack
+                sx={{
+                  marginLeft: 5,
+                  marginRight: 4,
+                  marginBottom: 1,
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  sx={{
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    color: "#00CCCC",
+                    marginTop: 10,
+                    marginBottom: 2,
+                  }}
+                >
+                  Chatbot
+                </Typography>
+              </Stack>
+              <Box
+                sx={{
+                  padding: 2,
+                  textAlign: "center",
+                  background: "#003333",
+                  marginInlineStart: 0.5,
+                  marginInlineEnd: 0.5,
+                  height: "70vh",
+                  // border: 1,
+                  // borderColor: "#D499FF",
+                  borderRadius: 5,
+                  width: { xs: "90vw", sm: "90vw", md: "50vw", lg: "50vw" },
+                }}
+              >
+                <Box
+                  sx={{
+                    padding: 1,
+                    margin: 2,
+                    textAlign: "center",
+                    marginInlineStart: 0.5,
+                    marginInlineEnd: 0.5,
+                    height: "80%",
+                    // border: 1,
+                    // borderColor: "#D499FF",
+                    borderRadius: 3,
+                    overflow: "auto",
+                  }}
+                >
+                  {arrQA.map((qa) => (
+                    <Box>
+                      <Box
+                        sx={{
+                          marginY: 1,
+                          borderRadius: 5,
+                          padding: 1,
+                          width: "100%",
+                          display: "flex",
+                          flexDirection: "row-reverse",
+                        }}
+                      >
+                        <Box
+                          sx={{
+                            backgroundColor: "white",
+                            marginY: 1,
+                            marginX: 1,
+                            borderRadius: 100,
+                            padding: 1,
+                            width: "max-content",
+                            height: "max-content",
+                          }}
+                        >
+                          You
+                        </Box>
+                        <Box
+                          sx={{
+                            backgroundColor: "white",
+                            marginY: 1,
+                            borderRadius: 5,
+                            padding: 1,
+                            width: "max-content",
+                            maxWidth: "70%",
+                            minWidth: "20%",
+                          }}
+                        >
+                          {qa.question}
+                        </Box>
+                      </Box>
+                      <Box
+                        sx={{
+                          marginY: 1,
+                          borderRadius: 5,
+                          padding: 1,
+                          width: "100%",
+                          display: "flex",
+                          flexDirection: "row",
+                        }}
+                      >
+                        <Box
+                          sx={{
+                            backgroundColor: "white",
+                            marginY: 1,
+                            marginX: 1,
+                            borderRadius: 100,
+                            padding: 1,
+                            width: "max-content",
+                            height: "max-content",
+                          }}
+                        >
+                          Bot
+                        </Box>
+                        <Box
+                          sx={{
+                            backgroundColor: "white",
+                            marginY: 1,
+                            borderRadius: 5,
+                            padding: 1,
+                            width: "max-content",
+                            maxWidth: "70%",
+                            minWidth: "20%",
+                          }}
+                        >
+                          {qa.answer}
+                        </Box>
+                      </Box>
+                    </Box>
+                  ))}
+                </Box>
+                <Box
+                  sx={{
+                    padding: 1,
+                    margin: 2,
+                    textAlign: "center",
+                    background: "#D4CCFF",
+                    marginInlineStart: 0.5,
+                    marginInlineEnd: 0.5,
+                    height: "auto",
+                    // border: 1,
+                    // borderColor: "#D499FF",
+                    borderRadius: 3,
+                  }}
+                >
+                  <Stack direction={"row"} spacing={2}>
+                    <TextField
+                      id="outlined-basic"
+                      variant="outlined"
+                      onChange={(e) => handleInputQuestion(e.target.value)}
+                      sx={{
+                        width: "100%",
+                        "& .MuiInputBase-input.Mui-disabled": {
+                          WebkitTextFillColor: "white",
+                        },
+                      }}
+                    />
+                    <Button onClick={handleClickRegex}>SEND</Button>
+                  </Stack>
+                </Box>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
       </Grid>
     </>
   );
